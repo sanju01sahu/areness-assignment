097cbff3ac06a14eb6739ad7dacc025e5548d036
Sanjay <sanju01.sahu@gmail.com>
2024-10-18 13:52:49 +0530

    backend server debug

---
+++ frontend/src/Redux/Store.js
@@ -0,0 +1,11 @@
+import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
+import { reducer as AuthReducer } from "./AuthRedux/reducer";
+import { reducer as SignUpReducer } from "./SignUpRedux/reducer";
+import thunk from "redux-thunk";
+
+const rootReduder = combineReducers({
+  AuthReducer,
+  SignUpReducer,
+});
+
+export const store = legacy_createStore(rootReduder, applyMiddleware(thunk));
